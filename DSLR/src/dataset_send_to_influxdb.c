70ac945d99aebfe5a00c5163840940ae281b637c
Juhani Kauppi <juhanikauppi@hotmail.com>
2021-09-11 10:31:24 +0300

    Content of the influxdb_line improved.

--- DSLR/src/dataset_send_to_influxdb.c
+++ DSLR/src/dataset_send_to_influxdb.c
@@ -6,41 +6,93 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 00:20:24 by jkauppi           #+#    #+#             */
-/*   Updated: 2021/09/09 15:53:12 by jkauppi          ###   ########.fr       */
+/*   Updated: 2021/09/11 10:25:21 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dslr.h"
 
+static void	influxdb_line_format_according_to_rules(char *const influxdb_line)
+{
+	const char	*line_original;
+	const char	*ptr;
+	size_t		i;
+
+	line_original = ft_strdup(influxdb_line);
+	ptr = line_original;
+	i = 0;
+	while (*ptr)
+	{
+		if (ft_isspace(*ptr))
+			influxdb_line[i++] = '\\';
+		influxdb_line[i++] = *ptr;
+		ptr++;
+	}
+	return ;
+}
+
+static const char	*influxdb_line_create(
+									const char *const hogwarts_house,
+									const char *const hogwarts_subject,
+									const char *const value_string)
+{
+	time_t		utc_time_ms;
+	char		*endptr;
+	char		*influxdb_line;
+	double		value;
+	const char	*measurement;
+	char		*tags;
+	const char	*fields;
+	char		*timestamp;
+
+	influxdb_line = ft_memalloc(sizeof(*influxdb_line) * SEND_REC_BUF_MAX_SIZE);
+	tags = ft_memalloc(sizeof(*tags) * SEND_REC_BUF_MAX_SIZE);
+	timestamp = ft_memalloc(sizeof(*timestamp) * SEND_REC_BUF_MAX_SIZE);
+	value = strtod(value_string, &endptr);
+	utc_time_ms = ft_gettime();
+	measurement = ft_strdup("dataset_train");
+	ft_sprintf(tags, "hogwarts_subject=%s,Hogwarts\\ House=%s",
+		hogwarts_subject, hogwarts_house);
+	influxdb_line_format_according_to_rules(tags);
+	fields = ft_strjoin("value=", value_string);
+	ft_sprintf(timestamp, "%ld", utc_time_ms);
+	ft_sprintf(influxdb_line, "%s,%s %s %s", measurement, tags, fields,
+		timestamp);
+	return (influxdb_line);
+}
+
 void	dataset_send_to_influxdb(
 							t_tcp_connection *influxdb_connection,
 							const t_dataset *const dataset)
 {
-	time_t		utc_time_ms;
-	char		influxdb_field_string[SEND_REC_BUF_MAX_SIZE];
+	char		*endptr;
+	const char	*influxdb_line;
 	t_list		*elem;
 	const char	**value_array;
-	const char	*name;
-	double		value;
 	size_t		index;
-	char		*endptr;
+	const char	*hogwarts_subject;
+	const char	*hogwarts_house;
+	size_t		i;
 
 	elem = dataset->value_array_lst;
 	while (elem)
 	{
-		utc_time_ms = ft_gettime();
 		value_array = *(const char ***)elem->content;
 		index = ft_strtoi(value_array[0], &endptr, 10);
-		name = dataset->column_name_array[7];
-		value = strtod(value_array[7], &endptr);
-		FT_LOG_INFO("%lu %s=%f", index, name, value);
+		hogwarts_house = value_array[1];
+		if (!(index % 20))
+			FT_LOG_INFO("%lu", index);
+		i = 5;
+		while (++i < 19)
+		{
+			hogwarts_subject = dataset->column_name_array[i];
+			influxdb_line = influxdb_line_create(hogwarts_house,
+					hogwarts_subject, value_array[i]);
+			ft_influxdb_write(influxdb_connection, influxdb_line,
+				g_influxdb_token_array, NUMBER_OF_INFLUXDB_TOKENS);
+			ft_strdel((char **)&influxdb_line);
+		}
 		elem = elem->next;
-		ft_sprintf(influxdb_field_string,
-			"dataset_train,index=%lu used_percent=%f %ld", index, value, utc_time_ms);
-		ft_influxdb_write(influxdb_connection, influxdb_field_string,
-			g_influxdb_token_array, NUMBER_OF_INFLUXDB_TOKENS);
 	}
-	utc_time_ms = ft_gettime();
-	FT_LOG_INFO("Time: %ld", utc_time_ms);
 	return ;
 }
