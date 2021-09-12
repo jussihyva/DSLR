f2ae2d7871d44a4b6b7249f6fab4dbcaab3d2695
Juhani Kauppi <juhanikauppi@hotmail.com>
2021-09-13 01:34:28 +0300

    Influxdb line group implemented.

--- DSLR/src/dataset_send_to_influxdb.c
+++ DSLR/src/dataset_send_to_influxdb.c
@@ -6,30 +6,11 @@
 /*   By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 00:20:24 by jkauppi           #+#    #+#             */
-/*   Updated: 2021/09/13 01:03:09 by jkauppi          ###   ########.fr       */
+/*   Updated: 2021/09/13 01:30:36 by jkauppi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "dslr.h"
-
-// static void	influxdb_line_format_according_to_rules(char *const influxdb_line)
-// {
-// 	const char	*line_original;
-// 	const char	*ptr;
-// 	size_t		i;
-
-// 	line_original = ft_strdup(influxdb_line);
-// 	ptr = line_original;
-// 	i = 0;
-// 	while (*ptr)
-// 	{
-// 		if (*ptr == ' ')
-// 			influxdb_line[i++] = '\\';
-// 		influxdb_line[i++] = *ptr;
-// 		ptr++;
-// 	}
-// 	return ;
-// }
 
 static t_bool	validate_input(const char *const value_string)
 {
@@ -44,98 +25,6 @@ static t_bool	validate_input(const char *const value_string)
 		result = E_TRUE;
 	return (result);
 }
-
-// static const char	*influxdb_line_create(
-// 									const char *const hogwarts_house,
-// 									const char *const hogwarts_subject,
-// 									const char *const value_string)
-// {
-// 	time_t		utc_time_ms;
-// 	char		*endptr;
-// 	char		*influxdb_line;
-// 	double		value;
-// 	const char	*measurement;
-// 	char		*tags;
-// 	const char	*fields;
-// 	char		*timestamp;
-
-// 	if(validate_input(value_string))
-// 	{
-// 		influxdb_line = ft_memalloc(sizeof(*influxdb_line)
-// 				* SEND_REC_BUF_MAX_SIZE);
-// 		tags = ft_memalloc(sizeof(*tags) * SEND_REC_BUF_MAX_SIZE);
-// 		timestamp = ft_memalloc(sizeof(*timestamp) * SEND_REC_BUF_MAX_SIZE);
-// 		value = strtod(value_string, &endptr);
-// 		utc_time_ms = ft_gettime();
-// 		measurement = ft_strdup("dataset_train");
-// 		ft_sprintf(tags, "hogwarts_subject=%s,Hogwarts House=%s",
-// 			hogwarts_subject, hogwarts_house);
-// 		influxdb_line_format_according_to_rules(tags);
-// 		fields = ft_strjoin("value=", value_string);
-// 		ft_sprintf(timestamp, "%ld", utc_time_ms);
-// 		ft_sprintf(influxdb_line, "%s,%s %s %s", measurement, tags, fields,
-// 			timestamp);
-// 		ft_strdel((char **)&measurement);
-// 		ft_strdel(&tags);
-// 		ft_strdel((char **)&fields);
-// 		ft_strdel(&timestamp);
-// 	}
-// 	else
-// 		influxdb_line = NULL;
-// 	return (influxdb_line);
-// }
-
-// static const char	*influxdb_line_create1(
-// 									const char *const hogwarts_house,
-// 									const t_queue *const field_queue,
-// 									const size_t field_string_length_total)
-// {
-// 	const char	*field_string;
-// 	time_t		utc_time_ms;
-// 	char		*endptr;
-// 	char		*influxdb_line;
-// 	double		value;
-// 	const char	*measurement;
-// 	char		*tags;
-// 	const char	*fields;
-// 	char		*timestamp;
-
-// 	influxdb_line = NULL;
-// 	if (ft_is_queue_empty((t_queue *)field_queue))
-// 		fields = NULL;
-// 	else
-// 	{
-// 		fields = ft_memalloc(sizeof(*influxdb_line)
-// 				* field_string_length_total);
-// 		while (!ft_is_queue_empty((t_queue *)field_queue))
-// 		{
-// 			field_string = (const char *)ft_dequeue((t_queue *)field_queue);
-// 			if (*fields)
-// 				ft_strcat(fields, ',');
-// 			ft_strcat(fields, field_string);
-// 			ft_strdel(&field_string);
-// 		}
-// 		tags = ft_memalloc(sizeof(*tags) * SEND_REC_BUF_MAX_SIZE);
-// 		timestamp = ft_memalloc(sizeof(*timestamp) * SEND_REC_BUF_MAX_SIZE);
-// 		value = strtod(value_string, &endptr);
-// 		utc_time_ms = ft_gettime();
-// 		measurement = ft_strdup("dataset_train");
-// 		ft_sprintf(tags, "hogwarts_subject=%s,Hogwarts House=%s",
-// 			hogwarts_subject, hogwarts_house);
-// 		influxdb_line_format_according_to_rules(tags);
-// 		ft_sprintf(timestamp, "%ld", utc_time_ms);
-// 		influxdb_line = ft_memalloc(sizeof(*influxdb_line)
-// 				* (ft_strlen(measurement) + 1 + ft_strlen(tags) + 1
-// 				+ field_string_length_total + 1 + ft_strlen(timestamp)));
-// 		ft_sprintf(influxdb_line, "%s,%s %s %s", measurement, tags, fields,
-// 			timestamp);
-// 		ft_strdel((char **)&measurement);
-// 		ft_strdel(&tags);
-// 		ft_strdel((char **)&fields);
-// 		ft_strdel(&timestamp);
-// 	}
-// 	return (influxdb_line);
-// }
 
 static const char	*influxdb_line_create(
 									const char *const hogwarts_house,
@@ -194,6 +83,7 @@ void	dataset_send_to_influxdb(
 {
 	char			*endptr;
 	const char		*influxdb_line;
+	char			*influxdb_lines;
 	t_list			*elem;
 	const char		**value_array;
 	size_t			index;
@@ -204,9 +94,6 @@ void	dataset_send_to_influxdb(
 	t_queue			*field_queue;
 	size_t			field_string_length_total;
 
-	(void)result;
-	(void)influxdb_connection;
-	(void)influxdb_line;
 	field_queue = ft_queue_init();
 	elem = dataset->value_array_lst;
 	while (elem)
@@ -217,31 +104,38 @@ void	dataset_send_to_influxdb(
 		if (!(index % 20))
 			FT_LOG_INFO("%lu", index);
 		i = 5;
-		field_string_length_total = 0;
+		field_string_length_total = -1;
 		while (++i < 19)
 		{
 			hogwarts_subject = dataset->column_name_array[i];
 			influxdb_line = influxdb_line_create(hogwarts_house,
 					hogwarts_subject, value_array[i], index);
-			// if (influxdb_line)
-			// {
-			// 	ft_enqueue(field_queue, (void *)influxdb_line);
-			// 	field_string_length_total += ft_strlen(influxdb_line) + 1;
-			// }
 			if (influxdb_line)
 			{
-				result = ft_influxdb_write(influxdb_connection, influxdb_line,
-						g_influxdb_token_array, NUMBER_OF_INFLUXDB_TOKENS);
-				if (!result)
-					FT_LOG_ERROR("Sending of data to an influxdb failed!");
-				ft_strdel((char **)&influxdb_line);
+				ft_enqueue(field_queue, (void *)influxdb_line);
+				field_string_length_total += ft_strlen(influxdb_line) + 1;
 			}
-			else
-				FT_LOG_WARN("%s %s (check line (dataset file) %lu)",
-					"Value is empty for", hogwarts_subject, index);
 		}
-		// influxdb_line_create1(hogwarts_house, field_queue,
-		// 	field_string_length_total);
+		influxdb_lines = ft_strnew(field_string_length_total);
+		while (!ft_is_queue_empty(field_queue))
+		{
+			influxdb_line = (const char *)ft_dequeue(field_queue);
+			if (*influxdb_lines)
+				ft_strcat(influxdb_lines, "\n");
+			ft_strcat(influxdb_lines, influxdb_line);
+			ft_strdel((char **)&influxdb_line);
+		}
+		if (influxdb_lines)
+		{
+			result = ft_influxdb_write(influxdb_connection, influxdb_lines,
+					g_influxdb_token_array, NUMBER_OF_INFLUXDB_TOKENS);
+			if (!result)
+				FT_LOG_ERROR("Sending of data to an influxdb failed!");
+			ft_strdel((char **)&influxdb_lines);
+		}
+		else
+			FT_LOG_WARN("%s %s (check line (dataset file) %lu)",
+				"Value is empty for", hogwarts_subject, index);
 		elem = elem->next;
 	}
 	ft_memdel((void **)&field_queue->in_stack);
