786aea292990b638989ea64f0da33667de6a34e5
Juhani Kauppi <juhanikauppi@hotmail.com>
2021-10-06 19:21:20 +0300

    Histogram and scatter included.

--- python/histogram.py
+++ python/histogram.py
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    describe.py                                        :+:      :+:    :+:    #
+#    histogram.py                                       :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: jkauppi <jkauppi@student.hive.fi>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2021/10/06 15:45:44 by jkauppi           #+#    #+#              #
-#    Updated: 2021/10/06 18:46:43 by jkauppi          ###   ########.fr        #
+#    Created: 2021/10/06 18:49:56 by jkauppi           #+#    #+#              #
+#    Updated: 2021/10/06 19:17:12 by jkauppi          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -27,38 +27,31 @@ def read_cmd_arguments():
 		input_params.update({"dataset_file": sys.argv[1]})
 	return (input_params)
 
-def describe(dataset_file):
+def histogram(dataset_file):
 	my_data = pd.read_csv(dataset_file)
-	print(my_data.columns)
 	hogwarts_subjects = my_data[["Arithmancy", "Astronomy", "Herbology", \
 		"Defense Against the Dark Arts", "Divination", 'Muggle Studies', \
        'Ancient Runes', 'History of Magic', 'Transfiguration', 'Potions', \
        'Care of Magical Creatures', 'Charms', 'Flying']]
-	describe_list = {}
-	describe_list.update({"Count": hogwarts_subjects.count()})
-	describe_list.update({"Mean": hogwarts_subjects.mean()})
-	describe_list.update({"Std": hogwarts_subjects.std()})
-	describe_list.update({"Median": hogwarts_subjects.median()})
-	describe_list.update({"Min": hogwarts_subjects.min()})
-	describe_list.update({"Max": hogwarts_subjects.max()})
-	describe_list.update({"1%": hogwarts_subjects.quantile(0.01)})
-	describe_list.update({"25%": hogwarts_subjects.quantile(0.25)})
-	describe_list.update({"50%": hogwarts_subjects.quantile(0.50)})
-	describe_list.update({"75%": hogwarts_subjects.quantile(0.75)})
-	describe_list.update({"99%": hogwarts_subjects.quantile(0.99)})
-	my_describe_df = pd.DataFrame(describe_list)
+	histogram_list = {}
+	histogram_list.update({"Count": hogwarts_subjects.count()})
+	histogram_list.update({"Mean": hogwarts_subjects.mean()})
+	histogram_list.update({"Std": hogwarts_subjects.std()})
+	histogram_list.update({"Median": hogwarts_subjects.median()})
+	histogram_list.update({"Min": hogwarts_subjects.min()})
+	histogram_list.update({"Max": hogwarts_subjects.max()})
+	histogram_list.update({"1%": hogwarts_subjects.quantile(0.01)})
+	histogram_list.update({"25%": hogwarts_subjects.quantile(0.25)})
+	histogram_list.update({"50%": hogwarts_subjects.quantile(0.50)})
+	histogram_list.update({"75%": hogwarts_subjects.quantile(0.75)})
+	histogram_list.update({"99%": hogwarts_subjects.quantile(0.99)})
+	my_histogram_df = pd.DataFrame(histogram_list)
 	# print(hogwarts_subjects["Defense Against the Dark Arts"])
 	# hogwarts_subjects = hogwarts_subjects.fillna(hogwarts_subjects.mean())
 	# print(hogwarts_subjects["Defense Against the Dark Arts"])
-	print(my_describe_df)
-	# f, ax = plt.subplots()
-	# ax.bar(x=my_data["Index"],
-	# 	height=my_data["Arithmancy"],
-    #    	color="blue")
-	# ax.set(title="Hogwarts")
-	# plt.show()
+	pd.DataFrame.hist(hogwarts_subjects)
+	plt.show()
 
 if __name__ == "__main__":
 	input_params = read_cmd_arguments()
-	describe(input_params["dataset_file"])
-
+	histogram(input_params["dataset_file"])
